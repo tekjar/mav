53a02ab92763695d3b0294d5765dad7dc5098aa3
tekjar <k.teza1@gmail.com>
2018-03-19 06:13:25 +0530

    Parse xml for code generation

--- examples/generate.rs
+++ examples/generate.rs
@@ -1,5 +1,12 @@
 extern crate mav;
 
+use mav::Mav2Rust;
+use std::path::Path;
+
 fn main() {
-    let xml = mav::try();
+    let spec_path = Path::new("common.xml");
+    let code_path = Path::new(("src/mavlink.rs"));
+
+    let mav2rust = Mav2Rust::new(&spec_path);
+    mav2rust.generate(code_path);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,19 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 extern crate syn;
+extern crate treexml;
 
 pub mod mavlink;
 
 use std::fs::File;
 use std::io::BufReader;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::path::Path;
 
+use treexml::{Document, Element};
 use serde_json::{Error, Value};
 use syn::Ident;
+use quote::Tokens;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MavLink {
@@ -25,34 +29,99 @@ struct Mav {
     mavlink: MavLink,
 }
 
-pub fn generate() {
-    let schema_file = File::open("common.json").unwrap();
-    let schema: Value = serde_json::from_reader(schema_file).unwrap();
+pub struct Mav2Rust {
+    version: Element,
+    enums: Element,
+    messages: Element,
+}
 
-    let enums = &schema["mavlink"]["enums"]["enum"][0]["entry"];
-    let enums = enums.as_array().unwrap();
+impl Mav2Rust {
+    pub fn new(mavlink_spec: &Path) -> Mav2Rust {
+        let mut schema_file = File::open(mavlink_spec).unwrap();
+        let mut schema = String::new();
 
-    for i in enums.iter() {
-        println!("{:?}", i["_name"]);
+        let _ = schema_file.read_to_string(&mut schema).unwrap();
+        let schema = schema.as_bytes();
+
+        let tree = Document::parse(schema).unwrap();
+        let mut root = tree.root.unwrap();
+
+        let version = root.children.remove(0);
+        let enums = root.children.remove(0);
+        let messages = root.children.remove(0);
+
+        Mav2Rust {
+            version,
+            enums,
+            messages,
+        }
+    }
+
+    pub fn generate(&self, output_file: &Path) {
+        let mut mavlink = File::create(output_file).unwrap();
+        let mut root = vec![];
+        let children = self.messages.children.clone();
+
+        for mut message in children {
+            // println!("{:?}", message);
+
+            // break;
+            let name = message.attributes.get("name").unwrap();
+            let description = message.children.remove(0).text.unwrap();
+
+            let name = Ident::from(name.as_ref());
+            let mut fields = vec![];
+
+            for field in message.children {
+                let doc = field.text.unwrap();
+                let attributes = field.attributes;
+                let member = attributes["name"].clone();
+                let typ = rustify(&attributes["type"].clone());
+
+                println!("member = {}, type = {}", member, typ);
+                
+                let member = Ident::from(member);
+                let typ = Ident::from(typ);
+                fields.push(quote! {
+                    #[doc = #doc]
+                    #member: #typ
+                });
+            }
+            let tokens = quote!(
+                    #[doc = #description]
+                    pub struct #name {
+                        #(#fields,)*
+                    }
+
+
+            );
+
+            root.push(tokens);
+        }
+
+        let tokens = quote!(#(#root)*);
+        mavlink.write_all(tokens.to_string().as_bytes()).unwrap();
     }
 }
 
-pub fn try() {
-    let mut mavlink = File::create("src/mavlink.rs").unwrap();
-
-    let names = vec![
-        Ident::from("Altitude"),
-        Ident::from("Gps"),
-        Ident::from("Gyro"),
-    ];
-
-    let kinds = vec!["u8", "u8", "u8"];
-
-    let tokens = quote!(
-        # ( struct # names {
-            value: #kinds
-        })*
-    );
-
-    mavlink.write_all(tokens.to_string().as_bytes());
+fn rustify(typ: &str) -> String {
+    match typ {
+        "uint8_t" | "uint8_t_mavlink_version" => "u8".to_owned(),
+        "uint16_t" => "u16".to_owned(),
+        "uint32_t" => "u32".to_owned(),
+        "uint64_t" => "u64".to_owned(),
+        "int8_t"  => "i8".to_owned(),
+        "int16_t" => "i16".to_owned(),
+        "int32_t" => "i32".to_owned(),
+        "int64_t" => "i64".to_owned(),
+        //TODO: Verify float default size
+        "float" => "f32".to_owned(),
+        //FIXME
+        _ if typ.contains("char[") => "u8".to_owned(),
+        _ if typ.contains("array[") => "u8".to_owned(),
+        _ if typ.contains("int8_t[") => "u8".to_owned(),
+        _ => panic!("Invalid type {:?} to convert", typ)
+    }
 }
+
+
